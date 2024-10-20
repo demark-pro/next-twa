6c33215dbc7e1cb18954181798333c510cc1b9da
Mark Davydkin <mark.davydkin@yandex.ru>
2024-10-20 20:19:32 +0300

    feat: rename NextTWAProviderReturn to UseNextTWAReturn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export {
   NextTWAProvider,
   NextTWAProviderProps,
-  NextTWAProviderReturn,
+  UseNextTWAReturn,
 } from './core/context/context';
 export { useNextTWA } from './core/context/use-next-twa';
 export { WebApp } from './core/twa-types';

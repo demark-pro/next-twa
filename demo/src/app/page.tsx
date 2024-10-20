8d885ac31b97f9d3a59993a0dbb5048a07c43c94
Mark Davydkin <mark.davydkin@yandex.ru>
2024-10-20 22:52:43 +0300

    feat: basic example

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -1,22 +1,53 @@
 'use client';
-
-import { NextTWAProvider, useNextTWA } from 'next-twa';
+import {
+  NextTWAProvider,
+  useNextTWA,
+  OnStartAppHandler,
+  useTelegramInitData,
+} from 'next-twa';
 
 const App = () => {
-  const app = useNextTWA();
+  const { isReady, app, startAppValue } = useNextTWA();
+  const initData = useTelegramInitData();
 
-  console.log(app);
-  return <div></div>;
+  if (!isReady) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <p>StartAppValue: {startAppValue}</p>
+      <p>
+        {initData &&
+          Object.entries(initData).map(([key, value]) => (
+            <p key={key}>
+              {key}: {JSON.parse(value)}
+            </p>
+          ))}
+      </p>
+      <button
+        onClick={() => {
+          app?.showAlert('Hi!');
+        }}
+      >
+        Show Alert
+      </button>
+    </div>
+  );
 };
 
 export default function Home() {
+  const handleStartApp: OnStartAppHandler = startParam => {
+    if (startParam) {
+      // Do something. redirect or extract data from `start_param`
+      // The returned value will be written to `startAppValue`.
+
+      return { foo: 'bar' };
+    }
+
+    return 'string or anything';
+  };
+
   return (
-    <NextTWAProvider
-      onStartApp={startParam => {
-        return 'test';
-      }}
-      useBackButton={true}
-    >
+    <NextTWAProvider onStartApp={handleStartApp} useBackButton={true}>
       <App />
     </NextTWAProvider>
   );

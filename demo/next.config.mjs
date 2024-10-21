db6a52034af261141677cc3def7c61d35d66dfc3
Mark Davydkin <mark.davydkin@yandex.ru>
2024-10-21 21:13:50 +0300

    fix: add basePath for prod

--- demo/next.config.mjs
+++ demo/next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-twa' : ''
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-twa' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/next-twa' : ''
+
 };
 
 export default nextConfig;

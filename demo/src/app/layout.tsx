7c28408242e0f9e4f48e8cc37ecbc0597f3e6733
Mark Davydkin <mark.davydkin@yandex.ru>
2024-10-21 03:51:50 +0300

    feat: remove NextTWAProvider to providers

--- demo/src/app/layout.tsx
+++ demo/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
-import { OnStartAppHandler } from 'next-twa';
-import { NextTWAProvider } from 'next-twa';
+import Providers from './providers/providers';
 
 export const metadata: Metadata = {
   title: 'Next TWA Example',
@@ -12,26 +11,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const handleStartApp: OnStartAppHandler = startParam => {
-    if (startParam) {
-      // Do something. redirect or extract data from `start_param`
-      // The returned value will be written to `startAppValue`.
-
-      return { foo: 'bar' };
-    }
-
-    return 'string or anything';
-  };
-
   return (
     <html lang="en">
       <body>
-        <NextTWAProvider
-          // onStartApp={handleStartApp}
-          useBackButton={true}
-        >
-          {children}
-        </NextTWAProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

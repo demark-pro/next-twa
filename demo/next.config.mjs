7546fb5563ddaf65fc47ca2734132bed9c453ac7
Mark Davydkin <mark.davydkin@yandex.ru>
2024-10-21 21:58:29 +0300

    feat: delete assetPrefix and basePath temp

--- demo/next.config.mjs
+++ demo/next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-twa' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/next-twa' : ''
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/next-twa' : '',
+    // basePath: process.env.NODE_ENV === 'production' ? '/next-twa' : ''
 
 };
 
